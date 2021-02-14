4338ac6a118876a1e8104d45cdf1aeba84cde258
Andre Hochi <andrehochiwork@gmail.com>
2021-02-14 20:35:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,7 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  array.count do |element|
-    element.kind_of?(String) && element.count == 0
+  array.count do {|element| element.size == 0}
+    element.kind_of?(String)
   end
 end
